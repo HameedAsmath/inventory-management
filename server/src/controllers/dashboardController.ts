d4399633ad3cc9e432508b0a122d7b26bde66a4c
HameedAsmath <hameed@HameedAsmaths-MacBook-Pro.local>
2026-01-13 21:09:45 +0530

    initial commit

---
+++ server/src/controllers/dashboardController.ts
@@ -0,0 +1,53 @@
+import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
+
+export const getDashboardMetrics = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const popularProducts = await prisma.products.findMany({
+      take: 15,
+      orderBy: {
+        stockQuantity: "desc",
+      },
+    });
+    const salesSummary = await prisma.salesSummary.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const purchaseSummary = await prisma.purchaseSummary.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const expenseSummary = await prisma.expenseSummary.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const expenseByCategoryRaw = await prisma.expenseByCategory.findMany({
+      take: 5,
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const expenseByCategorySummary = expenseByCategoryRaw.map((item) => ({
+      ...item,
+      amount: item.amount.toString(),
+    }));
+    res.json({
+      popularProducts,
+      salesSummary,
+      purchaseSummary,
+      expenseSummary,
+      expenseByCategorySummary,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving dashboard metrics" });
+  }
+};
